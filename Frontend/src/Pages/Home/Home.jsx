5e8b8793c4975b683fd7b6357f4df4251cb65b87
SarojKumarMallik <sarojkumarmallik94@gmail.com>
2025-12-22 13:10:13 +0530

    Our Experts Design Complete

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Subscribe from '../../Components/Subscribe/Subscribe'
 
 const Home = () => {
   return (
-    <div>
-        <h1></h1>
-    </div>
+    <>
+       <Subscribe/> 
+    </>
   )
 }
 
